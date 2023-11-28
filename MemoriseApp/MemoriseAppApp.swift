ad6c7e76a7f35ac5baafbeda3562a6f333b3a336
Sreekanth <sreekanthps28@gmail.com>
2023-11-28 15:03:14 +0000

    Initial Commit

---
+++ MemoriseApp/MemoriseAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoriseAppApp.swift
+//  MemoriseApp
+//
+//  Created by SREEKANTH PS on 28/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct MemoriseAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
